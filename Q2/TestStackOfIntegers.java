2a01e29da0a7838faf7047406b4a35dc4f99b518
AloyLooWJ <97747020+AloyLooWJ@users.noreply.github.com>
2022-03-28 12:33:09 +0800

    Add files via upload

---
+++ Q2/TestStackOfIntegers.java
@@ -0,0 +1,17 @@
+public class TestStackOfIntegers
+{
+    public static void main(String [] args)
+    {
+        StackOfIntegers stack = new StackOfIntegers(20);
+
+        for (int i = 0; i < 20; i++)
+        {
+            stack.push(i);
+        }
+
+        while (!stack.empty())
+        {
+            System.out.print(stack.pop() + " ");
+        }
+    }
+}
